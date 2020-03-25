fd3328094d61a68962c88dbe95ec0b24fd7666c8
crclz <crucialize@foxmail.com>
2020-03-25 09:33:59 +0800

    Login test ok

---
+++ Leopard.API.Test/Single/LoginTest.cs
@@ -0,0 +1,33 @@
+﻿using Leopard.Domain;
+using Org.OpenAPITools.Api;
+using Org.OpenAPITools.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Leopard.API.Test.Single
+{
+	public class LoginTest
+	{
+		[Fact]
+		async Task LoginNormal()
+		{
+			var username = XUtils.GetRandomString(12);
+			var password = "asda1dsas";
+
+			// register
+			var usersApi = new UsersApi(TestConfig.BaseUrl);
+			var res = await usersApi.RegisterAsync(new RegisterModel(username, password, "x", ""));
+			Assert.NotNull(res);
+			Assert.NotNull(res.Id);
+
+			// login
+			var accessApi = new AccessApi(TestConfig.BaseUrl);
+			var accessRes = await accessApi.LoginAsync(new LoginModel(username, password));
+			Assert.NotNull(accessRes);
+			Assert.NotNull(accessRes.AccessToken);
+		}
+	}
+}
