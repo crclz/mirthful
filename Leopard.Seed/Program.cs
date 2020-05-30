2bc052aca2606af348f3e0960e7c51efb39fb5ea
crclz <crucialize@foxmail.com>
2020-05-30 10:56:11 +0800

    Random text ok

---
+++ Leopard.Seed/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Leopard.Seed
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var randomTextGenerator = new RandomText();
+
+			for(int i = 1; i <= 10; i++)
+			{
+				Console.WriteLine(randomTextGenerator.Generate(100, 200));
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ Leopard.Seed/RandomText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Leopard.Seed
+{
+	class RandomText
+	{
+		private Random Random = new Random();
+
+		private string SourceText;
+
+		public RandomText()
+		{
+			SourceText = File.ReadAllText("random-text.txt");
+			SourceText = SourceText.Replace("\r\n", " ");
+			SourceText = SourceText.Replace("\n", " ");
+		}
+
+		public string Generate(int requiredLength)
+		{
+			int start = Random.Next(0, SourceText.Length - 1);
+
+			StringBuilder builder = new StringBuilder();
+
+			for (int i = 0; i < requiredLength; i++)
+			{
+				char c = SourceText[(start + i) % SourceText.Length];
+				builder.Append(c);
+			}
+
+			return builder.ToString();
+		}
+
+		public string Generate(int minlength, int maxlength)
+		{
+			int requiredLength = Random.Next(minlength, maxlength);
+
+			return Generate(requiredLength);
+		}
+	}
+}
